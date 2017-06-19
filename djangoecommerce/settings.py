886ec7efb36d5e34e28282fe9f4cd6b4ec2338ce
brenothales <breno.thales@gmail.com>
2017-06-18 21:41:41 -0300

    ajustando whitenoise e implmentações de teste unitários no projeto

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = ')mnrzwad@-n#eh33oc960@=!d3--jd(p&bbw=+@g!k$vu5)5!h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
