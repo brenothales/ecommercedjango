f2b8cad9a7e681012869bb2393a87291702c8fd9
brenothales <breno.thales@gmail.com>
2017-06-20 11:17:37 -0300

    implmentando based class e paginações nos produtos e categorias

--- core/views.py
+++ core/views.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
+from django.views.generic import View, TemplateView
 
 
 from .forms import ContactForm
 
-# Create your views here.
 
-def index(request):
-    return render(request, "index.html")
+
+# def index(request):
+#     return render(request, "index.html")
+
+class IndexView(TemplateView):
+# utilizando class based views
+    template_name = 'index.html'
+
+index = IndexView.as_view()
 
 
 def contact(request):
