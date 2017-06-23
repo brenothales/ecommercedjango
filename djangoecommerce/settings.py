4ea4dccc507b94d24d7ad6b1ee220b2fffcc7caf
brenothales <breno.thales@gmail.com>
2017-06-23 11:03:25 -0300

    User authentication in Django¶

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -144,14 +144,17 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 EMAIL_HOST = ''
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
-DEFAULT_FROM_EMAIL = 'admin@djangoecommerce.com'
+DEFAULT_FROM_EMAIL = 'admin@gmail.com'
 
 # auth
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_URL = 'logout'
 AUTH_USER_MODEL = 'accounts.User'
-
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.ModelBackend'
+)
 try:
     from .local_settings import *
 except ImportError:
