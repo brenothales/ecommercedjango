1bb3ee1d0453828d62130a118dd894661810e75f
brenothales <breno.thales@gmail.com>
2017-06-27 11:19:25 -0300

    Trabalhando com Sessions, Messages e implementando Carrinho de compras através do modulo Checkout

--- core/views.py
+++ core/views.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import View, TemplateView, CreateView
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 from .forms import ContactForm
 
@@ -22,11 +23,16 @@ index = IndexView.as_view()
 
 
 def contact(request):
+
     success = False
     form = ContactForm(request.POST or None)
     if form.is_valid():
         form.send_mail()
         success = True
+
+    elif request.method == 'POST':
+        messages.error(request, 'Formulário inválido')
+
     context = {
         'form': form,
         'success': success
